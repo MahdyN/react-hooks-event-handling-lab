2cba49266b232c06d8aba96f98a843b24b26c59e
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-12-15 08:19:58 -0500

    commit tree 478f7bcc32ed632e4f01e66cc87091e9a996eb96

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    
+    function passwordChange() {
+        console.log("Entering password...")
+    }
+
+    return(
+        <input 
+        type="password"
+        onChange={passwordChange}
+         />
+    )
+}
+
+export default Keypad;
+
+
+
